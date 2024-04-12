550ee130deb30045e9798f1e7211b15e6f271d2a
Melvin <melvinnnamani@gmail.com>
2024-04-12 12:53:45 +0100

    Update global.css, consts.ts, and BaseHead.astro

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,8 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "CR3ST Digital Agency";
-export const SITE_DESCRIPTION = "CR3ST: Where Tech Meets Brilliance. We build the future of Business with Innovative ERP, Web Design & Mobile Apps.";
+export const SITE_DESCRIPTION =
+  "CR3ST: Where Tech Meets Brilliance. We build the future of Business with Innovative ERP, Web Design & Mobile Apps.";
 export const SITE_URL = "https://www.cr3st.com/";
-export const KEYWORDS = "";
+export const KEYWORDS =
+  "cr3st digital agency, cr3st agency, crest, crest digital agency, crest agency, cr3st web design, cr3st development services, cr3st marketing solutions, cr3st branding agency, cr3st seo services, cr3st mobile app development, cr3st erp solutions, cr3st creative agency, digital agency cr3st, web design by cr3st, cr3st creative solutions, cr3st online marketing, cr3st graphic design, digital agency, web design, mobile app development, branding services, erp solutions, online presence, custom website design, creative branding, digital marketing agency, seo services, social media management, e-commerce solutions, graphic design services, ux/ui design, software development, technology solutions, creative agency, website optimization, content marketing, responsive web design";
