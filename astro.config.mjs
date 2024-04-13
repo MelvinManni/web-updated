1166585bcab060f77bce1a5acbc92a17bb1a055d
Melvin <melvinnnamani@gmail.com>
2024-04-14 00:00:04 +0100

    Update Astro configuration and dependencies for Storyblok integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,27 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind({
-    applyBaseStyles: true,
-  })]
+  site: "https://cr3st.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      applyBaseStyles: true,
+    }),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      apiOptions: {
+        region: "eu",
+      },
+    }),
+  ],
 });
